db16286850843aa425f7daf0c6ff088f80954c3a
nick <runner.emerson@gmail.com>
2015-07-03 20:03:26 +0800

    add test

--- Makefile
+++ Makefile
@@ -3,8 +3,10 @@ MAKE = make
 all:
 	cd src/; ${MAKE}
 	cd benchmark/; ${MAKE}
+	cd test/; ${MAKE}
 
 clean:
 	rm -f *.a  ldb_bench
 	cd src/; ${MAKE} clean
 	cd benchmark/; ${MAKE} clean
+	cd test/; ${MAKE} clean
---
+++ test/test_kv.c
@@ -0,0 +1,30 @@
+#include "t_kv.h"
+#include "ldb_define.h"
+
+#include <assert.h>
+#include <time.h>
+#include <string.h>
+
+
+int main(int argc, char* argv[]){
+  ldb_context_t *context = ldb_context_create("/tmp/testdb", 128, 64);
+  assert(context != NULL);
+  char *ckey = "key1";
+  char *cval = "val1";
+  ldb_slice_t *key1 = ldb_slice_create(ckey, strlen(ckey));
+  ldb_slice_t *val1 = ldb_slice_create(cval, strlen(cval));
+  ldb_meta_t *meta1 = ldb_meta_create(0, 0, time(NULL)); 
+
+  assert(string_set(context, key1, val1, meta1) == LDB_OK);
+
+  ldb_slice_destroy(val1);
+  ldb_meta_destroy(meta1); 
+
+  assert(string_get(context, key1, &val1)==LDB_OK); 
+
+  ldb_slice_destroy(val1);
+  ldb_slice_destroy(key1);
+
+  ldb_context_destroy(context);  
+  return 0;
+}
